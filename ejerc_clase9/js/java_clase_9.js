7187b254a3c2a67839abee455345059b0ede420e
Fede Biaggini <fede_biaggini@hotmail.com>
2022-11-20 20:52:33 -0300

    primer commit e9

---
+++ ejerc_clase9/js/java_clase_9.js
@@ -0,0 +1,16 @@
+/*mi primer js!*/
+
+window.alert ('Disculpen la precariedad, estoy rindiendo finales.');
+
+/*variables*/
+
+//let pais = "españa";
+//let continente = "europa";
+//let antiguedad = "2021";
+//let pais_y_continente = pais + " " + continente;
+
+//pais = "mexico";
+//continente = "latinoamerica";
+
+//console.log(pais, continente, antiguedad);
+//alert(pais_y_continente);
